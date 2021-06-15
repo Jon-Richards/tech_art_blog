86d5f8cddedb9cba613a6ce0f5cc6e76efa53ce0
Jon Richards <talljon@gmail.com>
2021-06-15 04:54:25 +0000

    copy: Begin styling common elements.

---
+++ client/src/components/copy/css_custom_property_table.ts
@@ -0,0 +1,15 @@
+import {LitElement, html, css} from 'lit-element';
+
+export class CSSPropDoc extends LitElement {
+  static get styles() {
+    return css`
+
+    `;
+  }
+  
+  render() {
+    return html`
+
+    `;
+  }
+}
