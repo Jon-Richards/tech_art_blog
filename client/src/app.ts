c11dad07ffa062fe73c75287c9e6e980b87004e0
Jon <talljon@gmail.com>
2021-07-17 11:39:08 -0400

    card-component: Initial setup.

--- client/src/app.ts
+++ client/src/app.ts
@@ -2,7 +2,9 @@ import './stylesheets/app.scss';
 import {Panel} from './components/panel';
 import {Heading} from './components/heading';
 import {Table} from './components/table';
+import {Card} from './components/card';
 
 customElements.define('jr-panel', Panel);
 customElements.define('jr-heading', Heading);
 customElements.define('jr-table', Table);
+customElements.define('jr-card', Card);
---
+++ client/src/components/card/card.ts
@@ -0,0 +1,84 @@
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  css,
+  CSSResult,
+  PropertyDeclarations,
+} from 'lit-element';
+
+export interface Image {
+  href: string;
+  alt: string;
+}
+
+/**
+ * Renders a Card used to describe something and link to another page.
+ */
+export class Card extends LitElement {
+  private description: string;
+  private image: string;
+
+  /**
+   * Defines the component's reactive properties.
+   */
+  static get properties(): PropertyDeclarations {
+    return {
+      description: {type: String},
+      image: {type: String},
+    };
+  }
+
+  /** The constructor. */
+  constructor() {
+    super();
+    this.description = '';
+    this.image = '';
+  }
+
+  /**
+   * Compiles the CSS used by the card.
+   */
+  static get styles(): CSSResult {
+    return css`
+      *. *::before, *::after {
+        box-sizing: border-box;
+      }
+
+      :host {
+        display: inline-block;
+      }
+
+      jr-panel {
+        --panel__padding: 0;
+      }
+
+      .image {
+        max-width: 100%;
+        height: auto;
+      }
+      
+      .image img {
+        max-width: 100%;
+        height: auto;
+      }
+    `;
+  }
+
+  /**
+   * Renders the card's HTML.
+   * @returns The card's compiled html.
+   */
+  render(): TemplateResult {
+    return html`
+      <jr-panel>
+        <div class="image">
+          <img src="${this.image}">
+        </div>
+        <p>
+          ${this.description}
+        </p>
+      </jr-panel>
+    `;
+  }
+}
